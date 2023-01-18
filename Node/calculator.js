133bce21c7862e4c4b28c8834081ab6dbd3ea57b
Rishabh Tripathi <rishabh29072001@gmail.com>
2023-01-18 13:16:41 +0530

    Node Learning

---
+++ Node/calculator.js
@@ -0,0 +1,15 @@
+// const add = (a,b) => a+ b;
+// const sub = (a,b) => a-b;
+// module.exports.add = add;
+// module.exports.sub = sub;
+// const a =1;
+let a = 5;
+exports.b  = 5;
+// exports.sub = sub;
+
+// module.exports = class StudentDetails {
+//     constructor() {
+//       this.name = "Rahul";
+//       this.rollNo = 20;
+//     }
+//   }
---
+++ Node/index.js
@@ -0,0 +1,20 @@
+// const greetings = (name) =>{
+//     console.log(`Hello ${name}`);
+// }
+// greetings("Raju");
+// greetings("Abhi");
+
+const calculator = require("./calculator");
+console.log(calculator);
+// const {add,sub} = calculator;
+// console.log(add(6,3));
+// console.log(sub(2,1));
+
+
+// const StudentDetails = require("./calculator.js");
+
+// const studentDetails = new StudentDetails();
+
+// console.log(studentDetails.name);
+
+// console.log(studentDetails.score);
